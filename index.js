a5f11c030f07411c6cb8b65b882f5822cbc75fd2
steveoh <sgourley@utah.gov>
2024-06-12 17:33:38 -0600

    chore: add formatting

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ async function run() {
 
     const httpClient = new http.HttpClient(
       'service-now-work-notes-github-action',
-      [new auth.BasicCredentialHandler(username, password)]
+      [new auth.BasicCredentialHandler(username, password)],
     );
 
     const url = `https://${instanceName}.service-now.com/api/now/table/${tableName}/${systemId}`;
@@ -38,7 +38,7 @@ async function run() {
 
     core.info(`Action run Id: ${github.context.runId}`);
     core.info(
-      `API request: GET /repos/${owner}/${repo}/actions/runs/${runId}/approvals`
+      `API request: GET /repos/${owner}/${repo}/actions/runs/${runId}/approvals`,
     );
 
     const { data } = await octokit.request(
@@ -47,13 +47,15 @@ async function run() {
         owner,
         repo,
         run_id: runId,
-      }
+      },
     );
 
     core.info(`Approvals: ${data.length}`);
 
     if (data.length === 0) {
-      core.setFailed(`No approvals found for runId: ${runId}. Does this environment require approvals? Check your environments in settings: https://github.com/${owner}/${repo}/settings/environments`);
+      core.setFailed(
+        `No approvals found for runId: ${runId}. Does this environment require approvals? Check your environments in settings: https://github.com/${owner}/${repo}/settings/environments`,
+      );
 
       return;
     }
@@ -64,7 +66,7 @@ async function run() {
     const comments = lastAttempt.comment;
 
     core.info(
-      `Approver: ${lastAttempt.user.login}, comment: ${lastAttempt.comment}`
+      `Approver: ${lastAttempt.user.login}, comment: ${lastAttempt.comment}`,
     );
     core.endGroup();
 
@@ -89,7 +91,7 @@ async function run() {
 
     if (response.statusCode != 200) {
       core.setFailed(
-        `request failed:${response.statusCode}, ${response.result}`
+        `request failed:${response.statusCode}, ${response.result}`,
       );
     }
     core.endGroup();
